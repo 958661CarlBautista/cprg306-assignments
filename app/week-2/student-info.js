62422666df7fe0dd9175f0f645ef505e0bc1e42f
958661CarlBautista <CarlEzekiel.Bautista@edu.sait.ca>
2025-09-15 19:59:13 -0600

    Updated root page and finished week 2 assignment
    Root Page: Added an assignment section and removed the previous NextJS code that was provided by Vercel.
    Week-2: Completed assignment, added Student Info.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Name: Carl Ezekiel Bautista</p>
+      <p>GitHub Repository: {}
+      <Link href="https://github.com/958661CarlBautista/cprg306-assignments">https://github.com/958661CarlBautista/cprg306-assignments</Link>
+      </p>
+    </div>
+  );
+}
